048c44f5489b573e1392476e740ef4ec4a2b1376
Sayyed Shozib Abbas <shozib.abbas@deaftawk.com>
2022-08-12 14:48:25 +0500

    Remove unused slice in redux

--- teamway_pt_web_app/src/redux/store.js
+++ teamway_pt_web_app/src/redux/store.js
@@ -1,23 +1,16 @@
 import {configureStore} from '@reduxjs/toolkit';
 import {setupListeners} from '@reduxjs/toolkit/query';
 import {quizApi} from './Quiz.api';
-import {QuizSlice} from './Quiz.slice';
-// import {ocrApi} from "./OCR.api";
-// import {DrawerSlice} from "./Drawer.slice";
 
 export const store = configureStore({
 	reducer: {
 		[quizApi.reducerPath]: quizApi.reducer,
-		[QuizSlice.name]: QuizSlice.reducer,
-		// [ocrApi.reducerPath]: ocrApi.reducer,
-		// [DrawerSlice.name]: DrawerSlice.reducer,
 	},
 	middleware: (getDefaultMiddleware) =>
 		getDefaultMiddleware({
 			serializableCheck: false,
 		})
 			.concat(quizApi.middleware)
-	// .concat(ocrApi.middleware)
 });
 
 setupListeners(store.dispatch);
